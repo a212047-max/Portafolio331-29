40b399dea67074ac0b8b037752cd83f868e56295
a212047-max <a.212047@imbc.edu.mx>
2026-09-24 09:30:36 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,75 +1,58 @@
 ﻿using System;
-// Espacio de nombres
-namespace CS3
+namespace CS4
 {
-    //Clase principal
     class Program
     {
-        //Función principal
         static void Main(string[] args)
         {
-            //Sesión 6: Operadores
-            // Declaración e inicialización 
-            int a = 1;
-            int b = 2;
-            int resultado = 0;
-            // 1. Operadores aritméticos
-            // a. Suma: +
-            resultado = a + b;
-            Console.WriteLine($"Suma: {resultado}");
-            // b. Resta: -
-            resultado = a - b;
-            Console.WriteLine($"Resta: {resultado}");
-            // c. Multiplicación: *
-            resultado = a * b;
-            Console.WriteLine($"Multiplicación: {resultado}");
-            // d. División: /
-            resultado = a / b;
-            Console.WriteLine($"División: {resultado}");
-            // e. Resto (módulo): %
-            resultado = a % b;
-            Console.WriteLine($"Residuo: {resultado}");
-            // Incrementos y decrementos
-            resultado += 9;
-            resultado -= 5;
-            Console.WriteLine($"Resultado: {resultado}");
-            /*
-            2. Operadores comparativos
-            a. Igualdad: ==
-            b. Diferencia: !=
-            c. Menor que: <
-            d. Mayor que: >
-            e. Menor o igual que: <=
-            f. Mayor o igual que: >=
-            */
-            // Sesión 7: Operadores comparativos
-            bool m = false;
-            m = 4 ==10;
-            Console.WriteLine($"Igualdad: {m}");
-            m = 5 != 5;
-            Console.WriteLine($"Diferencia: {m}");
-            m = 5 > 4;
-            Console.WriteLine($"Mayor que: {m}");
-            m = 4 < 5;
-            Console.WriteLine($"Menor que: {m}");
-            // Menor o igual que
-            // 1. Escribir una expresión que de como resultado true.
-            m = 3 <=9;
-            Console.WriteLine($"Menor o igual que: {m}");
-            // Mayor o igual que
-            // 2. Escribir una expresión que de como resultado false
-            m = 4 >= 5;
-            Console.WriteLine($"Mayor o igual que: {m}");
-            // 3. Operadores lógicos
-            // a. Y (AND): &&
-            //b. O (OR): ||
-            bool e = false; // Entrada 1
-            bool f = true; // Entrada 2
-            bool d = false; // Resultado
-            d = e && f;
-            Console.WriteLine($"Y: {d}");
-            d = e || f;
-            Console.WriteLine($"O: {d}");
-        } //Termino de la funcion principal
-    } //Termino de la clase principal
-} // Termino del espacio de nombres
+            // Sesión 7: Estructuras selectivas
+            // a. Simple: Instrucción if
+            int a = 0;
+            if (a == 0) // Si el valor de "a" es igual a 1, entonces...
+            {
+                // Bloque de instrucciones
+                Console.WriteLine("El valor de 'a' es igual a 0.");
+                a += 1;
+                Console.WriteLine($"a: {a}");
+            }
+            // b. Doble: Instrucciones if-else
+            // c. Múltiple: Instrucciones if-else if-else
+            // Bloque de instrucciones
+           // Sesión 8: Estructuras selectivas: dobles y múltiples
+           // 1. Estructura selectiva doble (instrucción if-else)
+           // Declaración e inicialziación
+           bool foco = false; // A la variable "foco", se le asigna, inicialmente, false.
+           if (foco == true) // Si el valor de la variable "foco" es true, entonces...
+           {
+               Console.WriteLine("El foco está encendido."); // ...impresión del estado del foco: encendido.
+           }
+           else // En caso contrario, entoces...
+           {
+               Console.WriteLine("El foco está apagado."); // ...impresión del estado del foco: apagado.
+           }
+           // 2. Estructura selectiva múltiple (instrucción if-else if-else)
+           // Declaración e inicialización
+           int salón = 332; // A la variable "salón, se le asigna, inicialmente, 331.
+           if (salón == 331) // Si el valor de la variable "salón" es 331, entonces...
+           {
+               Console.WriteLine("Exactas"); // ...impresión de especialidad: "Exactas".
+           }
+           else if (salón == 332 || salón == 333) // Si no es igual a 331, pero sí a 332 o a 333, entonces...
+           {
+               Console.WriteLine("Administrativas"); // ...impresión de especialidad: "Administrativas".
+           }
+           else if (salón == 334) // Si no es igual a 331, 332 o 333, pero sí a 334, entonces...
+           {
+               Console.WriteLine("Humanidades"); // ...impresión de especialidad: "Humanidades".
+           }
+           else if (salón == 335) // Si no es igual a 331, 332, 333, 333 o 334, pero sí a 335, entonces...
+           {
+               Console.WriteLine("Biológicas"); // ...impresión de especialidad: "Biológicas".
+           }
+           else // En cualquier otro caso, entonces...
+           {
+               Console.WriteLine("¡Salón no registrado!"); // ...impresión: "¡Salón no registrado!".
+           }
+        }
+    }
+}
